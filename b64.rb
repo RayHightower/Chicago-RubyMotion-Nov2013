7d99beeb45066c6f15960e60ddff47329c252c2d
Raymond T. Hightower <rth@wisdomgroup.com>
2013-11-14 19:31:02 -0600

    experimenting with base64

---
+++ b64.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+require 'base64'
+
+File.open('vim-screenshot.png', 'r') do|image_file|
+  puts Base64.encode64(image_file.read)
+end
+
+# Acknowledgement: http://ruby.about.com/od/advancedruby/ss/Base64-In-Ruby.htm
