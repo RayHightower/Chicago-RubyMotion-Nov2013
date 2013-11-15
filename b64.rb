cd1cc8075be0019fc0f9f6dccee937bc66807a12
Raymond T. Hightower <rth@wisdomgroup.com>
2013-11-14 22:14:56 -0600

    add rubymotion query app demo

--- b64.rb
+++ b64.rb
@@ -5,4 +5,4 @@ File.open('./rubymotion-sm.png', 'r') do|image_file|
   puts Base64.encode64(image_file.read)
 end
 
-# Acknowledgement: http://ruby.about.com/od/advancedruby/ss/Base64-In-Ruby.htm
+# Acknowledgement: From an article by Michael Morin. http://ruby.about.com/od/advancedruby/ss/Base64-In-Ruby.htm
